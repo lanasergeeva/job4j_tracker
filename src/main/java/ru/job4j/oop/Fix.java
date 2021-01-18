5dbc8ce0b4d22d9cec5533b9f8a47b1152d4c9ea
PoymaLana <marikakopina@gmail.com>
2021-01-18 17:16:04 +0300

    1.7. Конструктор [#437361]

---
+++ src/main/java/ru/job4j/oop/Fix.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+public class Fix {
+    private String desc;
+
+    public Fix(String text) {
+        desc=text;
+    }
+
+    public static void main(String[] args) {
+        Fix fix = new Fix("something");
+    }
+}
