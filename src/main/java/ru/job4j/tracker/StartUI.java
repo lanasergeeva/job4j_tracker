b8efc69503d50dabb36d8cb4ec183215332c7618
PoymaLana <marikakopina@gmail.com>
2021-01-21 16:09:31 +0300

    9. toString [#437382]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,9 +3,7 @@ package ru.job4j.tracker;
 public class StartUI {
 
     public static void main(String[] args) {
-        Tracker track = new Tracker();
         Item item = new Item();
-        System.out.println(track.add(item));
-        System.out.println(track.findById(4));
+        System.out.println(item);
     }
 }
