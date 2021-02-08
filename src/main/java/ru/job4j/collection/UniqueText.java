35baaaeecf8a8e2de5c16a7e303fd875edca3bfd
PoymaLana <marikakopina@gmail.com>
2021-02-08 16:54:27 +0300

    1. Уникальные задачи. [#437295]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection;
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<String>();
+        for (String first : origin) {
+            check.add(first);
+        }
+        for (String seconds : text) {
+            if (!check.contains(seconds)) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+}
