76a30ad64e8fe47856bd27c6b9520449055d6190
PoymaLana <marikakopina@gmail.com>
2021-02-19 13:28:17 +0300

    1. Экзамен. Коллекции. [#437274] исправила Article

--- src/main/java/ru/job4j/collection/Article.java
+++ src/main/java/ru/job4j/collection/Article.java
@@ -2,16 +2,28 @@ package ru.job4j.collection;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class Article {
-    public static boolean generateBy(String origin, String line) {
+    /*public static boolean generateBy(String origin, String line) {
         List<String> first = new ArrayList<>();
         List<String> second = new ArrayList<>();
         String regex = "((?!^)\\b)";
         first.addAll(Arrays.asList(origin.split(regex)));
         second.addAll(Arrays.asList(line.split(regex)));
         return first.containsAll(second) ? true : false;
+    }*/
+    public static boolean generateBy(String origin, String line) {
+        String regex = "((?!^)\\b)";
+        List<String> second = Arrays.asList(line.split(regex));
+        HashSet<String> hs = new HashSet<>(Arrays.asList(origin.split(regex)));
+        for (String s : second) {
+            if (!hs.contains(s)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
