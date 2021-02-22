2a8a86ea588c871c9d421c81c49b9d93a164b2f2
PoymaLana <marikakopina@gmail.com>
2021-02-22 21:02:59 +0300

    2. Список адресов [#437116]

---
+++ src/main/java/ru/job4j/stream/Profile.java
@@ -0,0 +1,19 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Profile {
+    private Address address;
+
+    public Address getAddress() {
+        return address;
+    }
+
+    List<Address> collect(List<Profile> profiles) {
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .collect(Collectors.toList());
+    }
+}
+
